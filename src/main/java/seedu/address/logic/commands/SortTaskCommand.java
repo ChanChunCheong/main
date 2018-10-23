3a51c454e9cec9c8c8c3c561d4b00d8b3153cedc
ChanChunCheong <chanchuncheong.cjz@gmail.com>
2018-10-24 04:05:35 +0800

    Teach the app to accept 'sort' arguments but still do nothing

--- src/main/java/seedu/address/logic/commands/SortTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/SortTaskCommand.java
@@ -6,12 +6,13 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_SORT;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 //import seedu.address.logic.parser.SortTaskCommandParser;
+import seedu.address.logic.parser.SortTaskCommandParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.task.Task;
 
 //@@author ChanChunCheong
-public class SortTaskCommand extends Command implements CommandParser{
+public class SortTaskCommand extends Command implements CommandParser {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": sort the tasks in the task book by preferred way. "
@@ -20,23 +21,55 @@ public class SortTaskCommand extends Command implements CommandParser{
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_SORT + "modules";
 
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "SortTask command not implemented yet";
+    //public static final String MESSAGE_NOT_IMPLEMENTED_YET = "SortTask command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "method: %1$s";
+    private final String method;
+
+    public SortTaskCommand() {
+        // Null so that it can be initialized in LogicManager
+        // Check in JUnit test
+        method = null;
+    }
+
+    /**
+     * Creates an DeferDeadlineCommand to add the specified {@code Task & @code Deadline}
+     */
+    public SortTaskCommand(String method) {
+        requireNonNull(method);
+        this.method = method;
+    }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        //throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, method));
     }
-
 
     @Override
     public Command parse(String arguments) throws ParseException {
-        return new SortTaskCommand();
+        return new SortTaskCommandParser().parse(arguments);
     }
-
 
     @Override
     public String getCommandWord() {
         return COMMAND_WORD;
     }
+
+    /*
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof SortTaskCommand)) {
+            return false;
+        }
+        // state check
+        SortTaskCommand e = (SortTaskCommand) other;
+        return method.equals(e.method);
+    }
+    */
 }
--- src/main/java/seedu/address/logic/parser/SortTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortTaskCommandParser.java
@@ -1,4 +1,46 @@
 package seedu.address.logic.parser;
 
-public class SortTaskCommandParser {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SORT;
+
+import java.util.stream.Stream;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.SortTaskCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+//@@ChanChunCheong
+/**
+ * Parses input arguments and creates a new SortTaskCommand object
+ */
+public class SortTaskCommandParser implements Parser<SortTaskCommand>{
+    public SortTaskCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_SORT);
+        /*
+        try {
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    SortTaskCommand.MESSAGE_USAGE), ive);
+        }
+        */
+        String method = argMultimap.getValue(PREFIX_SORT).orElse("");
+        //cannot have blank method
+        if (method == "") {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    SortTaskCommand.MESSAGE_USAGE));
+        }
+
+        return new SortTaskCommand(method);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
